b92006cfde493ec2d45e6dbff20e4886b4dd128f
Davi Reis <reis.davi11010@gmail.com>
2021-10-15 20:26:18 -0300

    changed the relay by a L298N (Motor Driver)

--- code/main/main.ino
+++ code/main/main.ino
@@ -5,30 +5,58 @@ float FLOAT_VARIABLES[] = {0}; // {front distance}
 
 // ---> ---> ---> ---> ---> ---> ---> ---> ---> ---> ---> --->
 
-int __MOTORS[] = {13, 12, 11}; // {relay, DCs, servo}
+int __MOTORS[] = {8, 7, 6, 5, 9, 4, 3}; // {In1, In2, In3, In4, EnA, EnB, Servo}
 Servo servo_motor;
 
 int SetupMotors() {
-    pinMode(__MOTORS[0], OUTPUT); pinMode(__MOTORS[1], OUTPUT);
-    servo_motor.attach(__MOTORS[2]);
+    pinMode(__MOTORS[4], OUTPUT);
+    pinMode(__MOTORS[5], OUTPUT);
+    pinMode(__MOTORS[0], OUTPUT);
+    pinMode(__MOTORS[1], OUTPUT);
+    pinMode(__MOTORS[2], OUTPUT);
+    pinMode(__MOTORS[3], OUTPUT);
+
+    servo_motor.attach(__MOTORS[6]);
+}
+
+int stop() {
+    digitalWrite(__MOTORS[0], LOW);
+    digitalWrite(__MOTORS[1], LOW);
+    digitalWrite(__MOTORS[2], LOW);
+    digitalWrite(__MOTORS[3], LOW);
+}
+int foward() {
+    digitalWrite(__MOTORS[0], HIGH);
+    digitalWrite(__MOTORS[1], LOW);
+    digitalWrite(__MOTORS[2], HIGH);
+    digitalWrite(__MOTORS[3], LOW);
+}
+int reverse() {
+    digitalWrite(__MOTORS[0], LOW);
+    digitalWrite(__MOTORS[1], HIGH);
+    digitalWrite(__MOTORS[2], LOW);
+    digitalWrite(__MOTORS[3], HIGH);
 }
 
 int MoveMotors() {
-    if (STATUS_VARIABLES[2] == 0) {
-        digitalWrite(__MOTORS[0], STATUS_VARIABLES[1] == 1 ? HIGH : LOW);
-        digitalWrite(__MOTORS[1], abs(STATUS_VARIABLES[1]));
-        servo_motor.write(90 * (1 + STATUS_VARIABLES[0]));
-    } else if (STATUS_VARIABLES[2] == 1) {
-        digitalWrite(__MOTORS[0], HIGH);
-        digitalWrite(__MOTORS[1], HIGH);
+    if (Serial.read() == "F") {
+        foward();
+    } else if (Serial.read == "B") {
+        reverse();
+    } else if (Serial.read == "L") {
+        servo_motor.write(0);
+    } else if (Serial.read == "R") {
         servo_motor.write(180);
+    } else {
+        stop();
+        servo_motor.write(90);
     }
-    delay(15);
+
 }
 
 // ---> ---> ---> ---> ---> ---> ---> ---> ---> ---> ---> --->
 
-int __SENSORS[] = {10, 9}; // {trigger, echo}
+int __SENSORS[] = {11, 10}; // {trigger, echo}
 
 int SetupSensors() {
 	pinMode(__SENSORS[0], OUTPUT);
